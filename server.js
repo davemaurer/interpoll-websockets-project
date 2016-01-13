9a7c715da77ac32aa9ace101ab4a85e7298cf626
David Maurer <davemaurermail@gmail.com>
2016-01-13 14:47:40 -0700

    install lodash express and socket.io. set up server. create socket object in client. create io object in server.

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(app)
+  .listen(port, function() {
+    console.log('Listening on port ' + port + '.');
+  });
+
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has connected.');
+});
+
+module.exports = server;
