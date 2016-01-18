b9d00775ac599311644653bd12ec63160b534e63
David Maurer <davemaurermail@gmail.com>
2016-01-18 15:17:17 -0700

    hooking up vote responses. not quite working yet.

--- public/client.js
+++ public/client.js
@@ -1,19 +1,32 @@
 var socket = io();
 var connectionCount = document.getElementById('connection-count');
+var pollId = window.location.pathname.split('/').slice(-1).pop();
 
 function activateAddOptionButton() {
   var addOptionButton = new AddOptionButton(
     document.querySelector('.add-option-button'),
     document.querySelector('.append-option')
   );
-console.log(addOptionButton);
   addOptionButton.activate();
+}
+
+$(document).ready(function() {
+  document.querySelector('choice-title').on('click', function() {
+    var vote = $(this).text();
+    castVote(vote);
+  })
+});
+
+function castVote(vote) {
+  socket.send('voteCast', {
+    pollId: pollId,
+    vote: vote,
+    voter: socket.id
+  })
 }
 
 socket.on('usersConnected', function(count) {
   connectionCount.innerText = 'Connected Users: ' + count;
 });
-
-
 
 activateAddOptionButton();
