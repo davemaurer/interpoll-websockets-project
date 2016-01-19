1536151cbd81050a2cb2a4c664da9641da9bb3e2
David Maurer <davemaurermail@gmail.com>
2016-01-19 01:27:26 -0700

    remove admin.js. not needed for now.

--- public/admin.js
+++
@@ -1,8 +0,0 @@
-var socket = io();
-var adminId = window.location.pathname.split('/').slice(-1).pop();
-var adminClosed = document.getElementById('buttonclose');
-
-socket.on('closePoll-' + adminId, function(message) {
-  closePollButton.remove();
-  adminClosed.innerText = message;
-});
